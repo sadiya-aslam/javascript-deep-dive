aefd826de8419cfe6ea910e42e0e0b96d200eca4
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-04-29 11:38:40 +0530

    Day 1 : understanding js variables,primitive data types

---
+++ Day1.js
@@ -0,0 +1,7 @@
+const sorcerer = "Yuji Itadori";
+let CurrentEnergyLevel = 500;
+let PassedSelection = true;
+let squad = null;
+let SpecialTech;
+console.log("Recruit "+sorcerer+ " has an energy level of "+CurrentEnergyLevel+"."+ " Passed selection: "+PassedSelection+".");
+
