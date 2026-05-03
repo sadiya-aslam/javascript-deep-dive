362971a11ed720773049b44c354851a474a5c845
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-05-03 11:11:53 +0530

    understanding loops

---
+++ Day4.js
@@ -0,0 +1,9 @@
+for(let i=1;i<=7;i++){
+    console.log("Defeated cursed spirit #"+i)
+}
+
+let stamina=100;
+ while(stamina>0){
+   console.log("Attacking! Stamina left: " + stamina)
+   stamina=stamina-25;
+ }
