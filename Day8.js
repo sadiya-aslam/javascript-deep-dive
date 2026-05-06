f83dd390d7477f8ce1b2c107b121befcd86bbd8f
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-05-06 12:41:42 +0530

    Day8: understanding DOM

---
+++ Day8.js
@@ -0,0 +1,7 @@
+let arena=document.getElementById('battlefield');
+arena.innerHTML="Domain Expansion: Infinite Void";
+arena.style.color='white';
+arena.style.backgroundColor='black';
+arena.style.padding='50px';
+arena.style.fontSize='32px';
+arena.style.textAlign='center';
