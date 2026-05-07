271eefdd689d7747e323e6cfb722e60831fbc49f
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-05-07 20:00:37 +0530

    event listeners

--- Day9.js
+++ Day9.js
@@ -1,5 +1,5 @@
-
-
+const triggerBtn=document.getElementById('cast-btn');
+const castDomain=()=>{
 let arena=document.getElementById('battlefield');
 arena.innerHTML="Domain Expansion: Infinite Void";
 arena.style.color='white';
@@ -7,7 +7,6 @@ arena.style.backgroundColor='black';
 arena.style.padding='50px';
 arena.style.fontSize='32px';
 arena.style.textAlign='center';
+}
 
-
-
-
+triggerBtn.addEventListener('click',castDomain)
