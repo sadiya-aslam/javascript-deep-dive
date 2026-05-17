d93b69dc2dc0b03ddbbf5deaac43d5ad19872b4d
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-05-17 18:01:00 +0530

    Day 15: Complete dynamic API search with error handling and URL encoding

--- Day15.js
+++ Day15.js
@@ -1,15 +1,32 @@
 let container=document.getElementById('anime-container')
+let searchBtn=document.getElementById('search-btn')
+let searchInput=document.getElementById('search-input');
 
-async function getAnimeData(){
-    let response=await fetch("https://api.jikan.moe/v4/anime?q=Attack on Titan&limit=1");
-    let rawData= await response.json();
-    console.log(rawData)
-  let animeTitle=rawData.data[0].title_english;
- 
-  let animeSynopsis=rawData.data[0].synopsis;
-    container.innerHTML=`<div >
-    <img src=${rawData.data[0].images.jpg.image_url}>
-    <h1>${animeTitle}</h1>
-    <p>${animeSynopsis}</p></div>`
+function renderAnimeData(rawData){
+  if(rawData.data && rawData.data.length > 0){
+   container.innerHTML=`<div >
+    <img src="${rawData.data[0].images.jpg.image_url}" alt="anime-image">
+    <h1>${rawData.data[0].title_english || rawData.data[0].title}</h1>
+    <p>${rawData.data[0].synopsis}</p></div>`
+  }
+    else{
+container.innerHTML=`<h1>Sorry, we couldn't find that anime!</h1>`
+    }
+
 }
-getAnimeData()
+async function getAnimeData(animeName){
+  let safeName=encodeURIComponent(animeName)
+    let response=await fetch(`https://api.jikan.moe/v4/anime?q=${safeName}&limit=1`);
+    let rawData= await response.json();
+   renderAnimeData(rawData)
+}
+
+
+function handleSearch(){
+let userInput=searchInput.value
+getAnimeData(userInput)
+searchInput.value=""
+}
+
+searchBtn.addEventListener('click',handleSearch)
+getAnimeData('Attack on Titan')
