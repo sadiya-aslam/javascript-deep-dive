12635508f634f5991ee5cbc4d2c29e654d6ac938
sadiya-aslam <sadiyaaslam0047@gmail.com>
2026-06-02 20:23:34 +0530

    Day 26-27: Implemented regex translation engine for real-time Markdown parsing

--- Day25.js
+++ Day25.js
@@ -1,6 +1,12 @@
-let preview=document.getElementById('preview');
-let editor=document.getElementById('editor');
+let preview = document.getElementById('preview');
+let editor = document.getElementById('editor');
 
-editor.addEventListener('input',(event)=>{
-preview.innerText=event.target.value
+editor.addEventListener('input', (event) => {
+    let text = event.target.value;
+    let formattedText = text.replace(/^# (.*$)/gim, '<h1>$1</h1>')      
+        .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') 
+        .replace(/\*(.*?)\*/g, '<em>$1</em>')            
+        .replace(/^> (.*$)/gim, '<blockquote>$1</blockquote>')
+        .replace(/\n/g, '<br>');
+    preview.innerHTML = formattedText;
 });
